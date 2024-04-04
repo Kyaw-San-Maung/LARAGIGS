4612dc4fbf5c9d587e2719553ba23299fb4bfac1
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-04-04 12:14:02 +0630

    feat: creating database for the app and adding model.

--- resources/views/listening.blade.php
+++ resources/views/listening.blade.php
@@ -1,6 +1,11 @@
  <h1>{{$headings}}</h1>
  
  @foreach ($posts as $post)
-    <h1>{{$post['title']}}</h1>
+ 
+    <h1>
+      <a href="/listing/{{$post['id']}}">
+         {{$post['title']}}
+      </a>
+   </h1>
     <p>{{$post['description']}}</p>
  @endforeach
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,18 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('listening', [
         "headings" => "Latest Listings",
-        "posts" => [
-            [
-            "id" => 1,
-            "title" => "Title one",
-            "description" => "This is first paragraph. My name is Kyaw San Maung who create this project."
-        ],
-        [
-            "id" => 2,
-            "title" => "Title two",
-            "description" => "This is second paragraph. My name is Kyaw San Maung who create this project."
-        ]
-        ]
+        "posts" => Listing::all()
+    ]);
+});
+
+//single listing
+Route:: get('/listing/{id}', function ($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
     ]);
 });
