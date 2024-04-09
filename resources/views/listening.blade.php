1a5186904917684cb917cd0e6bb6cab124de66f9
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-04-09 11:07:34 +0630

    feat:add dynamic styling for the code.

--- resources/views/listening.blade.php
+++ resources/views/listening.blade.php
@@ -7,46 +7,7 @@
   <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
  @foreach ($posts as $post)
- <div class="bg-gray-50 border border-gray-200 rounded p-6">
-                    <div class="flex">
-                        <img
-                            class="hidden w-48 mr-6 md:block"
-                            src="{{asset('images/no-image.png')}}"
-                            alt=""
-                        />
-                        <div>
-                            <h3 class="text-2xl">
-                                <a href="/listing/{{$post->id}}">{{$post->title}}</a>
-                            </h3>
-                            <div class="text-xl font-bold mb-4">{{$post->company}}</div>
-                            <ul class="flex">
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Laravel</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">API</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Backend</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Vue</a>
-                                </li>
-                            </ul>
-                            <div class="text-lg mt-4">
-                                <i class="fa-solid fa-location-dot"></i> {{$post->location}}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+ <x-listing-card :post="$post"/>
  @endforeach
 
    </div>
