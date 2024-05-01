fe4254905e029d3a19b143c7928fbdeca26209c4
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-05-02 00:40:57 +0630

    feat: add middlewear auth and guest

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ListingController::class, 'index']);
 
 
-Route::get('/listings/create', [ListingController::class, 'create']);
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
 
 //store listing data
 Route::post('/listings', [ListingController::class, 'store']);
@@ -17,13 +17,13 @@ Route::post('/listings', [ListingController::class, 'store']);
 Route::get('/listing/{listing}', [ListingController::class, 'show']);
 
 //Show Edit Form
-Route::get('/listing/{listing}/edit', [ListingController::class, 'edit']);
+Route::get('/listing/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
 
 //Update Listing
 Route::put('/listing/{listing}', [ListingController::class, 'update']);
 
 //Delete Listing
-Route::delete('/listing/{listing}', [ListingController::class, 'delete']);
+Route::delete('/listing/{listing}', [ListingController::class, 'delete'])->middleware('auth');
 
 //Show register/Create Form
 Route::get('/register', [UserController::class, 'create']);
@@ -32,10 +32,10 @@ Route::get('/register', [UserController::class, 'create']);
 Route::post('/users', [UserController::class, 'store']);
 
 //user logout
-Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
 
 //show login Form
-Route::get('/login', [UserController::class, 'login']);
+Route::get('/login', [UserController::class, 'login'])->name('login');
 
 //Log In User
 Route::post('/users/authenticate', [UserController::class, 'authenticate']);
