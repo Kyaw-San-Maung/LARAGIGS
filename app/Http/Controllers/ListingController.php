b120c887582bebec2f8a097863d10b07285bcfa4
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-05-02 11:35:36 +0630

    feat: add ownership to listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -48,6 +48,8 @@ class ListingController extends Controller
             $formFields['logo'] = $request->file('logo')->store('logos', 'public');
         }
 
+        $formFields['user_id'] = auth()->id();
+
         Listing::create($formFields);
 
         return redirect('/')->with("success", "Post created successfully!");
